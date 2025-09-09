a2c59f34bb73614d35cf3c9b216ba04774abc7ed
kedistde <76200367+kedistde@users.noreply.github.com>
2025-09-09 14:29:04 +0300

    Update schema.py

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,16 +1,9 @@
 import graphene
-import crm.schema
-
-class Query(crm.schema.Query, graphene.ObjectType):
-    # This will inherit from the CRM app's Query class
-    pass
-
-schema = graphene.Schema(query=Query)
-
-import graphene
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
     
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
+
+schema = graphene.Schema(query=Query)
